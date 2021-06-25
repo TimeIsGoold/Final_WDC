2190528b5c213e6ed695085d42a4ebc4b935b560
Hyon Woo Kang <87benzenes@gmail.com>
2021-06-25 17:55:12 +0900

    현우 commit

--- src/main/java/com/tig/wdc/PageTestController.java
+++ src/main/java/com/tig/wdc/PageTestController.java
@@ -8,11 +8,8 @@ public class PageTestController {
 
 	@RequestMapping("/test")
 	public String pageMove() {
-		
-<<<<<<< HEAD
-		return "/teacher/classManage/t_classAttendance";
-=======
+
 		return "teacher/t_main";
->>>>>>> branch 'master' of https://github.com/TimeIsGoold/Final_WDC.git
+
 	}
 }
