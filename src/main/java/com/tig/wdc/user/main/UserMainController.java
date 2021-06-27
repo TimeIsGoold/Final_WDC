352007c41065013a4252a8e8a09e9c182030b855
sook8878 <qpwlxk123@gmail.com>
2021-06-27 18:46:08 +0900

    user css 수정

--- src/main/java/com/tig/wdc/user/main/UserMainController.java
+++ src/main/java/com/tig/wdc/user/main/UserMainController.java
@@ -29,6 +29,6 @@ public class UserMainController {
 	@GetMapping("/classdetail")
 	public String classdetail() {
 		
-		return "user/mypage/coupon";
+		return "user/mypage/complateClass_detail";
 	}
 }
