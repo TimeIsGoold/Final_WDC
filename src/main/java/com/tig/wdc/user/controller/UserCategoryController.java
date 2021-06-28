949db125549c5b9082a0bcd2077ac8089e9b2a2f
sook8878 <qpwlxk123@gmail.com>
2021-06-28 14:32:23 +0900

    수정 완료

--- src/main/java/com/tig/wdc/user/controller/UserCategoryController.java
+++ src/main/java/com/tig/wdc/user/controller/UserCategoryController.java
@@ -1,7 +1,14 @@
 package com.tig.wdc.user.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tig.wdc.admin.model.dto.ClassDTO;
+import com.tig.wdc.user.model.service.UserCategoryService;
 
 /**
  * @author 민연준
@@ -12,4 +19,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("user/category/*")
 public class UserCategoryController {
 
+	private final UserCategoryService categoryService;
+	
+	@Autowired
+	public UserCategoryController(UserCategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+	
+	@GetMapping("whole")
+	public String ClassCategorySelect() {
+		
+		return null;
+	}
+	
 }
