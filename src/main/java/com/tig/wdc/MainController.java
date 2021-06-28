dec0c5ddf57276aa415b9154e09f159d5a9ed6f7
sora-zip <64796zip@gmail.com>
2021-06-28 15:34:14 +0900

    classDTO 사진추가

--- src/main/java/com/tig/wdc/MainController.java
+++ src/main/java/com/tig/wdc/MainController.java
@@ -1,23 +1,33 @@
 package com.tig.wdc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tig.wdc.user.model.dto.UserInfoDTO;
 import com.tig.wdc.user.model.service.UserClassService;
-import com.tig.wdc.user.model.service.UserInfoService;
 
+/**
+ * @author SORA
+ *
+ */
 @Controller
 @RequestMapping("/*")
 public class MainController {
 
+	private final UserClassService classService;
+	
+	@Autowired
+	public MainController(UserClassService classService) {
+		this.classService = classService;
+	}
+	
 	@GetMapping(value= {"/"})
 	public String main() {
 		
-//		private final UserClassService classService;
-		
-		
-		
+		ClassDTO classDTO = new ClassDTO();
+
 		return "user/main/main";
 	}
 	
