1f438e6999b33d36ae72e1dcf31a06e6ab1d38d8
sook8878 <qpwlxk123@gmail.com>
2021-06-29 10:36:33 +0900

    balanceServiceImpl 및 teacher alias 추가

--- src/main/java/com/tig/wdc/teacher/model/service/BalanceServiceImpl.java
+++ src/main/java/com/tig/wdc/teacher/model/service/BalanceServiceImpl.java
@@ -2,14 +2,25 @@ package com.tig.wdc.teacher.model.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.tig.wdc.model.dto.BalanceDTO;
 import com.tig.wdc.model.dto.PageInfoDTO;
 import com.tig.wdc.teacher.model.dao.TeacherBalanceMapper;
 
+@Service
 public class BalanceServiceImpl implements BalanceService {
 
+	
 	private TeacherBalanceMapper mapper;
 	
+	
+	@Autowired
+	public BalanceServiceImpl(TeacherBalanceMapper mapper) {
+		this.mapper = mapper;
+	}
+
 	@Override
 	public int selectBalanceCount() {
 		
