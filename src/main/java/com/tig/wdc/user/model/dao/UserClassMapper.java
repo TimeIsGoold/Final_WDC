58436abd4a097800f24070c58aa375e599516c16
sora-zip <64796zip@gmail.com>
2021-06-29 10:11:24 +0900

    클래스 이미지 추가, 메인 클래스 조회중

--- src/main/java/com/tig/wdc/user/model/dao/UserClassMapper.java
+++ src/main/java/com/tig/wdc/user/model/dao/UserClassMapper.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import com.tig.wdc.user.model.dto.UserClassDTO;
 
+/**
+ * @author SORA
+ * 클래스 정보 조회 매퍼
+ */
 public interface UserClassMapper {
 
 	public List<UserClassDTO> selectNewClassList();
---
+++ src/main/java/com/tig/wdc/user/model/dto/ClassApplyDTO.java
@@ -0,0 +1,87 @@
+package com.tig.wdc.user.model.dto;
+
+import java.sql.Date;
+
+/**
+ * @author SORA
+ * 클래스 신청 정보 DTO
+ */
+public class ClassApplyDTO {
+
+	private int aplNo;
+	private int ppl;
+	private Date aplDate;
+	private int userNo;
+	private String attStatus;
+	private int scheduleNo;
+
+	public ClassApplyDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClassApplyDTO(int aplNo, int ppl, Date aplDate, int userNo, String attStatus, int scheduleNo) {
+		super();
+		this.aplNo = aplNo;
+		this.ppl = ppl;
+		this.aplDate = aplDate;
+		this.userNo = userNo;
+		this.attStatus = attStatus;
+		this.scheduleNo = scheduleNo;
+	}
+
+	public int getAplNo() {
+		return aplNo;
+	}
+
+	public void setAplNo(int aplNo) {
+		this.aplNo = aplNo;
+	}
+
+	public int getPpl() {
+		return ppl;
+	}
+
+	public void setPpl(int ppl) {
+		this.ppl = ppl;
+	}
+
+	public Date getAplDate() {
+		return aplDate;
+	}
+
+	public void setAplDate(Date aplDate) {
+		this.aplDate = aplDate;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getAttStatus() {
+		return attStatus;
+	}
+
+	public void setAttStatus(String attStatus) {
+		this.attStatus = attStatus;
+	}
+
+	public int getScheduleNo() {
+		return scheduleNo;
+	}
+
+	public void setScheduleNo(int scheduleNo) {
+		this.scheduleNo = scheduleNo;
+	}
+
+	@Override
+	public String toString() {
+		return "ClassApplyDTO [aplNo=" + aplNo + ", ppl=" + ppl + ", aplDate=" + aplDate + ", userNo=" + userNo
+				+ ", attStatus=" + attStatus + ", scheduleNo=" + scheduleNo + "]";
+	}
+
+}
