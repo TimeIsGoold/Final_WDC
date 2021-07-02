753d201837012898fb6d9f42166ab527b1238022
haeseung-lee <dlgotmd93@gmail.com>
2021-07-02 18:03:23 +0900

    0702. 클래스 상세보기, 출석관리 작업중

--- src/main/java/com/tig/wdc/teacher/model/service/ClassRegistManageService.java
+++ src/main/java/com/tig/wdc/teacher/model/service/ClassRegistManageService.java
@@ -1,28 +1,35 @@
-package com.tig.wdc.teacher.model.dao;
-
-import java.util.HashMap;
-import java.util.List;
+package com.tig.wdc.teacher.model.service;
 
 import com.tig.wdc.model.dto.AttachMentDTO;
 import com.tig.wdc.model.dto.ClassPieceDTO;
 import com.tig.wdc.model.dto.CurriculumDTO;
+import com.tig.wdc.model.dto.RegularClassInfoDTO;
 import com.tig.wdc.user.model.dto.ClassDTO;
 import com.tig.wdc.user.model.dto.ScheduleDTO;
 
 /**
  * @author 이해승
- * 클래스등록용 Mapper
+ * 클래스등록 인터페이스
  */
-public interface ClassRegistMapper {
+public interface ClassRegistManageService {
 
+	//클래스정보 insert
 	int insertClassInfo(ClassDTO classInfo);
-
+	
+	//클래스 대표사진 insert
 	int insertTitlePicture(AttachMentDTO titlePicture);
 
+	//클래스 완성작 insert
 	int insertCompletePiece(ClassPieceDTO classPiece);
 
+	//클래스 커리큘럼 insert
 	int insertCurriculum(CurriculumDTO curriculum);
 
+	//클래스 스케쥴 
 	int insertSchedule(ScheduleDTO schedule);
 
+	//정규 클래스 정보
+	RegularClassInfoDTO selectRegularScheduleinfo(int clsNo);
+
+	
 }
